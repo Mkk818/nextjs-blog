03103f6757061e4c2330588d9f867e97db82cf83
Mikiko Noro <miriks818@gmail.com>
2022-03-18 21:22:19 +0900

    トップページ変更 ・日付表示をdate-fnsで処理 ・MDファイルをremark-htmlでhtmlに変換 ・Linkでブログのタイトルからブログのページに遷移

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,31 +1,38 @@
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from "../../styles/utils.module.css";
+import Layout from "../../components/layout";
+import {getAllPostIds, getPostData} from "../../lib/posts";
+import Date from "../../components/date";
 
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-    </Layout>
-  )
+export default function Post({postData}) {
+ return (
+  <Layout>
+   <head>
+    <title>{postData.title}</title>
+   </head>
+   <article>
+     <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+     <div className={utilStyles.lightText}>
+       <Date dateString={postData.date} />
+     </div>
+     <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+   </article>
+  </Layout>
+ );
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
-  return {
-    paths,
-    fallback: false
-  }
+ const paths = getAllPostIds();
+ return {
+  paths,
+  fallback: false,
+ };
 }
 
-export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
-  return {
-    props: {
-      postData
-    }
-  }
+export async function getStaticProps({params}) {
+ const postData = await getPostData(params.id);
+ return {
+  props: {
+   postData,
+  },
+ };
 }
