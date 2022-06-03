30b46a3a8ced6faf251c760e1afb5a823d83521f
Mikiko Noro <miriks818@gmail.com>
2022-06-03 20:14:43 +0900

    blog文字を真ん中に

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,77 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import { GetStaticProps } from 'next'
+import Head from 'next/head';
+import Layout, {siteTitle} from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import {getSortedPostsData} from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+import {GetStaticProps} from 'next';
 
 export default function Home({
-  allPostsData
+ allPostsData,
 }: {
-  allPostsData: {
-    date: string
-    title: string
-    id: string
-  }[]
+ allPostsData: {
+  date: string;
+  title: string;
+  id: string;
+ }[];
 }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hello, I&#39;m Mikiko Noro, studying HTML/CSS,JavaScript, and Next.js
-        </p>
-        <p>Please Follow My Tweet From<a href="https://twitter.com/MPrg2011">@MPrg2011</a>
-        </p>
-        <article className={utilStyles.twitterTL}>
-          <p>Twitter</p>
-          <a className="twitter-timeline" data-width="300" data-height="300" href="https://twitter.com/MPrg2011?ref_src=twsrc%5Etfw">Tweets by MPrg2011</a>
-          <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-        </article>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
+ return (
+  <Layout home>
+   <Head>
+    <title>{siteTitle}</title>
+   </Head>
+   <section className={utilStyles.headingMd}>
+    <div className={utilStyles.introduction}>
+     <p>
+      Hello, I&#39;m Mikiko Noro, studying HTML/CSS,JavaScript, and Next.js
+     </p>
+     <p>
+      Please Follow My Tweet From
+      <a href='https://twitter.com/MPrg2011'>@MPrg2011</a>
+     </p>
+    </div>
+    <article className={utilStyles.twitterTL}>
+     <p>Twitter</p>
+     <a
+      className='twitter-timeline'
+      data-width='300'
+      data-height='300'
+      href='https://twitter.com/MPrg2011?ref_src=twsrc%5Etfw'
+     >
+      Tweets by MPrg2011
+     </a>
+     <script
+      async
+      src='https://platform.twitter.com/widgets.js'
+      charSet='utf-8'
+     ></script>
+    </article>
+   </section>
+   <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+    <h2 className={utilStyles.headingLg}>Blog</h2>
+    <ul className={utilStyles.list}>
+     {allPostsData.map(({id, date, title}) => (
+      <li className={utilStyles.listItem} key={id}>
+       <Link href={`/posts/${id}`}>
+        <a>{title}</a>
+       </Link>
+       <br />
+       <small className={utilStyles.lightText}>
+        <Date dateString={date} />
+       </small>
+      </li>
+     ))}
+    </ul>
+   </section>
+  </Layout>
+ );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
+ const allPostsData = getSortedPostsData();
+ return {
+  props: {
+   allPostsData,
+  },
+ };
+};
